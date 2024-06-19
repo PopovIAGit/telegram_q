8e5a9a33aea488e651424d5d0d7fbdada9cfc604
Иван Андреевич Попов <myxxxwm@gmail.com>
2024-06-19 14:55:13 +0700

    первый коммит для инифиализации

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,47 @@
 
 <script>
 import { defineComponent } from 'vue'
+import { useAppStore } from 'stores/app'
+import { useHelperTablesStore } from 'stores/helperTables'
+import { useDialogStore } from 'stores/dialog'
 import WebSocketAsPromised from 'websocket-as-promised'
 
 
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+
+  async beforeMount() {
+
+    /** WS */
+    const wssServer = 'wss://sinthy.fvds.ru:3031';
+    this.$q.ws = new WebSocketAsPromised(wssServer, {
+      packMessage: data => JSON.stringify(data),
+      unpackMessage: data => JSON.parse(data),
+      attachRequestId: (data, requestId) => Object.assign({ id: requestId }, data),
+      extractRequestId: data => data && data.id
+    });
+    // Пробуем подключиться к серверу
+    try {
+      await this.$q.ws.open();
+    }
+    // Если ошибка подключения, то сообщаем и прерываем дальнейшее выполнение
+    catch (e) {
+      console.log(e);
+      this.$q.dialogStore.set({
+        show: true,
+        title: 'Ошибка',
+        text: 'Ошибка подключения к серверу ' + wssServer,
+        ok: {
+          color: 'red'
+        }
+      });
+      window['splash-screen'].classList.add('ready', 'error');
+      return;
+    }
+  },
+
+
+
 })
 </script>
