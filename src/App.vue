469cabb925bce94b23dd018828a28ce2b794b109
Иван Андреевич Попов <myxxxwm@gmail.com>
2024-06-19 14:51:01 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import WebSocketAsPromised from 'websocket-as-promised'
+
+
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
